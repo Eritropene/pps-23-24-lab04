0ab58ff4a4d57a77e8f42ea12986732cf6b9fcad
Michele Ravaioli <mikirava01@gmail.com>
2024-03-25 11:09:11 +0100

    task 1 done

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -21,12 +21,19 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
-    // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    private case class ComplNum(re: Double, im: Double)
+    opaque type Complex = ComplNum
+    def complex(re: Double, im: Double): Complex = ComplNum(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplNum(re, _) => re
+      def im(): Double = complex match
+        case ComplNum(_, im) => im
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplNum(re1, im1), ComplNum(re2, im2)) => ComplNum(re1 + re2, im1 + im2)
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplNum(re1, im1), ComplNum(re2, im2)) => ComplNum(re1 - re2, im1 - im2)
+      def asString(): String = complex match
+        case ComplNum(re, 0) => s"$re"
+        case ComplNum(0, im) => im + "i"
+        case ComplNum(re, im) => if (im > 0) then s"$re + ${im}i" else s"$re - ${-im}i"
